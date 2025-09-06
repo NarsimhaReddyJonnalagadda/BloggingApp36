6f9079682812e93d52207e9b72de37f3454e4ab1
Narsimha Reddy Jonnalagadda <jonnalagaddanarsimhareddy32@gmail.com>
2025-09-06 20:53:33 +0530

    Add files via upload

---
+++ SpringbootRealTimeMiniProjectBloggingApp36Application.java
@@ -0,0 +1,16 @@
+package com.excelr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRealTimeMiniProjectBloggingApp36Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRealTimeMiniProjectBloggingApp36Application.class, args);
+		
+		System.out.println("welcome");
+
+	}
+
+}
